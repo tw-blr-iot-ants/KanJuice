75e5fd9088ac2aa4ec265e0c911008d4edac5cb8
Thote Gowda <thotegowda.gr@gmail.com>
2015-09-30 00:35:39 +0530

    Added kannada text to most of the juices

--- app/src/main/java/com/example/kanjuice/JuiceDecorator.java
+++ app/src/main/java/com/example/kanjuice/JuiceDecorator.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class JuiceDecorator {
 
     private final HashMap<String, Integer> imageIdMap;
+    private final HashMap<String, Integer> kanTextMap;
 
     public JuiceDecorator() {
         imageIdMap = new HashMap<>();
@@ -25,9 +26,37 @@ public class JuiceDecorator {
         imageIdMap.put("salt lime soda", R.drawable.salt_lime_soda);
         imageIdMap.put("soda", R.drawable.soda);
         imageIdMap.put("sweet and salt lime soda", R.drawable.sweet_and_salt_lime_soda);
-        imageIdMap.put("amla", R.drawable.amla);
         imageIdMap.put("sweet lime soda", R.drawable.sweet_lime_soda);
         imageIdMap.put("watermelon", R.drawable.watermelon);
+
+        kanTextMap = new HashMap<>();
+        kanTextMap.put("amla", R.string.amla_kan);
+        kanTextMap.put("black jeera masala soda", R.string.black_jeera_masala_soda_kan);
+        kanTextMap.put("cucumber", R.string.cucumber_kan);
+        kanTextMap.put("ginger lime", R.string.ginger_lime_kan);
+        kanTextMap.put("ginger mint lime", R.string.ginger_mint_lime_kan);
+        kanTextMap.put("grapes", R.string.grapes_kan);
+        kanTextMap.put("jal jeera", R.string.jal_jeera_kan);
+        kanTextMap.put("kokum", R.string.kokum_kan);
+        kanTextMap.put("lime", R.string.lime_kan);
+        kanTextMap.put("mint lime", R.string.mint_lime_kan);
+        kanTextMap.put("mixed fruit", R.string.mixed_fruit_kan);
+        kanTextMap.put("muskmelon", R.string.muskmelon_kan);
+        kanTextMap.put("orange", R.string.orange_kan);
+        kanTextMap.put("pineapple", R.string.pineapple_kan);
+        kanTextMap.put("salt lime soda", R.string.salt_lime_soda_kan);
+        kanTextMap.put("soda", R.string.soda_kan);
+        kanTextMap.put("sweet and salt lime soda", R.string.sweet_and_salt_lime_soda_kan);
+        kanTextMap.put("sweet lime soda", R.string.sweet_lime_soda_kan);
+        kanTextMap.put("watermelon", R.string.watermelon_kan);
+        kanTextMap.put("banana", R.string.banana_kan);
+        kanTextMap.put("butter fruit", R.string.butter_fruit_kan);
+        kanTextMap.put("sapota", R.string.sapota_kan);
+        kanTextMap.put("apple", R.string.apple_kan);
+        kanTextMap.put("moosambi", R.string.moosambi_kan);
+        kanTextMap.put("grapes", R.string.grapes_kan);
+        kanTextMap.put("grape", R.string.grapes_kan);
+        kanTextMap.put("mango", R.string.mango_kan);
     }
 
     public int matchImage(String name) {
@@ -39,6 +68,10 @@ public class JuiceDecorator {
     }
 
     public int matchKannadaName(String name) {
-         return R.string.watermelon_kan;
+        Integer id = kanTextMap.get(name.toLowerCase().trim());
+        if (id == null) {
+            id = R.string.unknown_kan;
+        }
+        return id;
     }
 }
