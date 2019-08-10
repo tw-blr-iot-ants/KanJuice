e112bb264cb703b6fbd4bfdf2e3c008bc41cd8db
Prateek Jain <prateek.jain23995@yahoo.com>
2019-08-10 16:12:31 +0530

    Correcing authentication

--- app/src/main/java/com/example/kanjuice/KanJuiceApp.java
+++ app/src/main/java/com/example/kanjuice/KanJuiceApp.java
@@ -30,7 +30,6 @@ import static com.example.kanjuice.constants.KanjuiceUrls.*;
 
 @ReportsCrashes(formUri = KANJUICE_SERVER_LOG_URL)
 public class KanJuiceApp extends Application {
-    private static final String ENCRYPTION_KEY = "abcd1234";
     private RestAdapter restAdapter;
     private JuiceServer juiceServer;
 
@@ -39,31 +38,12 @@ public class KanJuiceApp extends Application {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
         ACRA.init(this);
-        try {
-            setupRestAdapter();
-        } catch (UnsupportedEncodingException | NoSuchAlgorithmException | InvalidKeyException |
-                NoSuchPaddingException | ShortBufferException | IllegalBlockSizeException |
-                BadPaddingException e) {
-            e.printStackTrace();
-        }
+        setupRestAdapter();
     }
 
-    private void setupRestAdapter() throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, ShortBufferException, BadPaddingException, IllegalBlockSizeException {
-
+    private void setupRestAdapter() {
         String basicAuth = "admin:123abc123";
-        byte[] input = basicAuth.getBytes("utf-8");
-
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        byte[] theDigest = md.digest(ENCRYPTION_KEY.getBytes("UTF-8"));
-        SecretKeySpec skc = new SecretKeySpec(theDigest, "AES/ECB/PKCS5Padding");
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, skc);
-
-        byte[] cipherText = new byte[cipher.getOutputSize(input.length)];
-        int ctLength = cipher.update(input, 0, input.length, cipherText, 0);
-        ctLength += cipher.doFinal(cipherText, ctLength);
-
-        final String basic = Base64.encodeToString(cipherText, Base64.DEFAULT);
+        final String basic = Base64.encodeToString(basicAuth.getBytes(), Base64.DEFAULT);
 
         restAdapter = new RestAdapter.Builder()
                 .setEndpoint(KANJUICE_SERVER_URL)
@@ -72,7 +52,7 @@ public class KanJuiceApp extends Application {
                 .setRequestInterceptor(new RequestInterceptor() {
                     @Override
                     public void intercept(RequestFacade request) {
-                        request.addHeader("Authorization", basic);
+                        request.addHeader("Authorization", "Basic " + basic);
                     }
                 })
                 .build();
